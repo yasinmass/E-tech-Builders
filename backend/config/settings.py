230c8f0a8653beb97507c32418e4484dbc6fceb4
Yasin Mass <yasinmass3069@gmail.com>
2026-06-10 16:22:36 +0530

    FIX : db file

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -80,7 +80,7 @@ ASGI_APPLICATION = "config.asgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3.db",
     }
 }
 
